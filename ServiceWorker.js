b12d7c7d9818995788b425d0f45607d7dcd893e8
MaugouMio <maugoumine@gmail.com>
2025-07-23 22:25:29 +0800

    Update to v3.0

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-GuessWord-1.0";
+const cacheName = "MaugouMio-GuessWord-3.0";
 const contentToCache = [
     "Build/GuessWordBuild.loader.js",
     "Build/GuessWordBuild.framework.js.unityweb",
